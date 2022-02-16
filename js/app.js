8a802e884090d709b04a782a8f4c29671af27d7f
khanmohammadimran <khanmohammadimran.i31@gmail.com>
2022-02-17 04:48:36 +0600

    Income, total expense and total balance updated

--- js/app.js
+++ js/app.js
@@ -1,3 +1,39 @@
 document.getElementById('calculate-btn').addEventListener('click', function(){
- console.log('button is clicked')
-});
+
+    // Income
+    const incomeInput = document.getElementById('my-income');
+    const incomeText = incomeInput.value;
+    const incomeAmount = parseInt(incomeText);
+    console.log(incomeAmount);
+
+    // Food Expenses 
+    const food = document.getElementById('food-expense');
+    const foodText = food.value;
+    const foodExpenses = parseFloat(foodText);
+
+    // Rent Expenses
+    const rent = document.getElementById('rent-expense');
+    const rentText = rent.value;
+    const rentExpenses = parseFloat(rentText);
+
+    // Clothes Expenses
+    const clothes = document.getElementById('clothes-expense');
+    const clothesText = clothes.value;
+    const clothesExpenses = parseFloat(clothesText);
+
+    // Total Expenses
+    const totalExpenseInput = document.getElementById('total-expense');
+    const totalExpenseText = totalExpenseInput.innerText;
+    const totalAmount = parseFloat(totalExpenseText);
+    const allExpense = foodExpenses + rentExpenses + clothesExpenses;
+    totalExpenseInput.innerText = allExpense;
+     console.log(allExpense);
+
+    // Total Balance
+    const totalBalanceInput = document.getElementById('total-balance');
+    const totalBalanceText = totalBalanceInput.innerText;
+    const totalBalanceAmount = parseFloat(totalBalanceText);
+    const totalBalance = incomeAmount - allExpense
+    totalBalanceInput.innerText = totalBalance;
+     console.log(totalBalanceInput);
+   });
